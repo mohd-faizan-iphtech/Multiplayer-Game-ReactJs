ba553c42579f31f84091ace6ffea922e2194ef98
Vikash Singh <133772183+vikash-singh-iphtech@users.noreply.github.com>
2023-10-11 15:36:37 +0530

    commit
    button change

--- client/src/Button.js
+++ client/src/Button.js
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 import { Icon } from '@iconify/react';
+import ReactiveButton from 'reactive-button';
+
+
 
 function Button({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
   const [buttonEnable,setButtonEnable]=useState(false)
+
+
+  const [state, setState] = useState('idle');
+
+  const onClickHandler = () => {
+    setState('loading');
+    sendMessage();
+
+    // send an HTTP request
+    setTimeout(() => {
+      setState('success');
+    }, 500);
+  };
+
 
   const sendMessage = async () => {
 setButtonEnable(true)
@@ -35,8 +52,30 @@ setButtonEnable(true)
 
   return (
     
-        <button className="btn btn-success" onClick={sendMessage} disabled={buttonEnable}> Click</button> 
+         <div className="container p-5">
+        <div class="card text-center">
+          <div class="card-header ">Game</div>
+          <div class="card-body">
+            <h5 class="card-title"> Let's play </h5>
+            <p class="card-text"> </p>
+  
+                 
+   <ReactiveButton
+      buttonState={state}
+      idleText="Submit"
+      loadingText="Loading"
+      successText="Done"
+      onClick={onClickHandler}
+      color="teal"
+      disabled={buttonEnable}
+    />
+                
+          </div>
+          <div class="card-footer text-body-secondary"></div>
+        </div>
+      </div>
+
   );
 }
 
-export default BUtton;
+export default Button
