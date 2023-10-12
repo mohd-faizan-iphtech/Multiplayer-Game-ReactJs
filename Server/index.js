d74cbade85ec704fb1e7a9228383e388e0468441
Faizan Ansari <124868842+mohd-faizan-iphtech@users.noreply.github.com>
2023-10-12 15:23:54 +0530

    add multiple user chances

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
 
 const players = [];
 let currentPlayerIndex = -1;
-const roomUsers = {}; 
+const roomUsers = {};
 const playerStatus = {};
 
 io.on("connection", (socket) => {
@@ -28,56 +28,51 @@ io.on("connection", (socket) => {
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
     if (!roomUsers[data]) {
       roomUsers[data] = 1;
-      } else {
+    } else {
       roomUsers[data]++;
-      }
-     
-      io.to(data).emit("user_count", roomUsers[data]);
-      playerStatus[socket.id] = "online";
-      io.emit("player_status", playerStatus);
-     
-  });
+    }
 
+    io.to(data).emit("user_count", roomUsers[data]);
+    playerStatus[socket.id] = "online";
+    io.emit("player_status", playerStatus);
+  });
 
   socket.on("send_message", (data) => {
     // if(players.length<3){
     //   socket.to(data.room).emit("receive_message", data);
     // }
     // else{
-    const currentPlayerIndex = players.findIndex(player => player.id === socket.id);
-  console.log("currentPlayerIndex",currentPlayerIndex)
+    const currentPlayerIndex = players.findIndex(
+      (player) => player.id === socket.id
+    );
+    console.log("currentPlayerIndex", currentPlayerIndex);
     const nextPlayerIndex = (currentPlayerIndex + 1) % players.length;
     const nextPlayer = players[nextPlayerIndex];
-  console.log(nextPlayerIndex)
+    console.log(nextPlayerIndex);
     // socket.to(data.room).emit("receive_message", data);
     // console.log(nextPlayer.id)
-    socket.to(nextPlayer.id).emit("enable_button",data);
+    socket.to(nextPlayer.id).emit("enable_button", data);
     //}
   });
-  
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
 
-    const index = players.findIndex(player => player.id === socket.id);
+    const index = players.findIndex((player) => player.id === socket.id);
     if (index !== -1) {
       players.splice(index, 1);
     }
     for (const room in roomUsers) {
       if (socket.rooms.has(room)) {
-      roomUsers[room]--;
-      io.to(room).emit("user_count", roomUsers[room]);
-      break;
+        roomUsers[room]--;
+        io.to(room).emit("user_count", roomUsers[room]);
+        break;
       }
-      }
-      playerStatus[socket.id] = "offline";
-      io.emit("player_status", playerStatus);
-     
-      });
+    }
+    playerStatus[socket.id] = "offline";
+    io.emit("player_status", playerStatus);
   });
-
- 
-
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
